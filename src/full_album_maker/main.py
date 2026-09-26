c5e189d742f7381ba59044bb43a5399e8ec7ebab
tonitarung099-creator <tonitarung099@gmail.com>
2026-09-26 19:19:04 +0700

    fix: enable cancellable render lifecycle

--- src/full_album_maker/main.py
+++ src/full_album_maker/main.py
@@ -7,6 +7,7 @@ from full_album_maker.visual_feature import install_visual_feature
 from full_album_maker.engine_hardening import install_engine_hardening
 from full_album_maker.source_integrity import install_source_integrity
 from full_album_maker.ui_hardening import install_ui_hardening
+from full_album_maker.render_lifecycle import install_render_lifecycle
 from full_album_maker.ui import run
 
 
@@ -17,6 +18,7 @@ install_visual_feature()
 install_engine_hardening()
 install_source_integrity()
 install_ui_hardening()
+install_render_lifecycle()
 
 if __name__ == "__main__":
     raise SystemExit(run())
